148f3ab77f85cf6c05aa41d691875ddcccad344a
arthurp1 <arthur.poot1@gmail.com>
2017-03-14 00:36:37 +0100

    sass added

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,7 +12,7 @@ class Navbar extends Component {
           <span className="icon-bar"></span>
           <span className="icon-bar"></span>
         </button>
-        <a className="navbar-brand" href="#">Brand</a>
+        <a className="navbar-brand" href="#">Betapp</a>
       </div>
       <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul className="nav navbar-nav">
